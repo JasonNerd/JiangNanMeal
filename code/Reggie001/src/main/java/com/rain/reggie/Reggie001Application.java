0396f01bf5121aae6037b60400a2abe3e976b330
JasonNerd <duan912rainbow@qq.com>
2023-12-19 22:09:34 +0800

    environment configure finished

---
+++ code/Reggie001/src/main/java/com/rain/reggie/Reggie001Application.java
@@ -0,0 +1,13 @@
+package com.rain.reggie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reggie001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Reggie001Application.class, args);
+    }
+
+}
