2f08b25ba68db3262c0a0217c02254e15297b8c5
JasonNerd <duan912rainbow@qq.com>
2023-12-22 10:43:44 +0800

    完成了登录校验

--- code/Reggie001/src/main/java/com/rain/reggie/controller/EmployeeController.java
+++ code/Reggie001/src/main/java/com/rain/reggie/controller/EmployeeController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
@@ -24,19 +23,40 @@ public class EmployeeController {
 
     /**
      * 员工登录
-     * @param request
-     * @param employee
-     * @return
+     * 1. 获取密码并进行md5加密
+     * 2. 查询数据库校验是否存在该用户(用户名)
+     * 3. 不存在则直接返回失败信息
+     * 4. 否则继续校验密码
+     * 5. 不匹配则直接返回失败信息
+     * 6. 否则继续校验用户状态
+     * 7. 若为禁用状态则返回失败信息
+     * 8. 否则登陆成功, 返回成功信息, 同时向请求中写入登录信息, 也即员工ID.
      */
     @PostMapping("/login")
     public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee){
-        return null;
+        String pwd = employee.getPassword();
+        pwd = DigestUtils.md5DigestAsHex(pwd.getBytes());
+        // 使用 QueryWrapper
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Employee::getUsername, employee.getUsername());
+        Employee result = employeeService.getOne(queryWrapper);
+        // 校验
+        if (result == null)
+            return R.error("用户不存在");
+        if (!result.getPassword().equals(pwd)) {
+            return R.error("密码错误");
+        }
+        if (result.getStatus() == 0) {
+            return R.error("账号已停用");
+        }
+        // 记录当前用户ID(登录状态)
+        request.getSession().setAttribute("employee", result.getId());
+        return R.success(result);
+        // http://localhost:8080/backend/page/login/login.html
     }
 
     /**
-     * 员工退出
-     * @param request
-     * @return
+     * 清除登录状态
      */
     @PostMapping("/logout")
     public R<String> logout(HttpServletRequest request){
